a530aadcc8c5b4c052ec11a85f6722ac484f083b
Thomas <tponthoreau@gmail.com>
2020-11-12 13:21:12 +0100

    chat-init

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+let socket;
+
+const Chat = ({location}) => {
+
+    const [name , setName] = useState('');
+    const [room, setRoom] = useState('');
+    const URL = 'localhost:8080'
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search)
+
+        socket = io(URL)
+
+        setName(name);
+        setRoom(room)
+        }, [URL, location.search])
+
     return ( 
         <>
             <h1>Chat</h1>
