73999affb983d6a5a6c0dde06838f8f52be017ea
Thomas <tponthoreau@gmail.com>
2020-11-12 12:18:24 +0100

    init

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import Join from "./components/Join"
+import Chat from "./components/Chat"
+
+const App = () => {
+  return (
+    <div className="App">
+      <Router>
+        <Route exact path="/" component={Join} />
+        <Route path="/chat" component={Chat} />  
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
+const PORT = process.env.PORT || 8080;
+const router = require('./router');
+
+const app = express();
+
+const server = http.createServer(app);
+const io = socketio(server)
+
+io.on('connection', (socket) => {
+    console.log('we have new connection');
+
+    socket.on('disconnect', () => {
+        console.log('user had left');
+    })
+})
+
+app.use(router)
+
+server.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
